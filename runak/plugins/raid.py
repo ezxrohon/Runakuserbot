3c20dedfc22caebe464756807577587eb7208ade
🦋ʀᴏʜ4ɴ <rohonkazi004@gmail.com>
2026-09-25 09:04:33 +0530

    Remove license and author comments from raid.py
    Removed header comments from raid.py file.

--- runak/plugins/raid.py
+++ runak/plugins/raid.py
@@ -1,23 +1,3 @@
-# =============================================================================
-#  CipherElite Userbot Plugin
-#
-#  Plugin Name:    raid
-#  Version:        1.0.0
-#  Author:         CipherElite Dev (@rishabhops)
-#  Repository:     https://github.com/rishabhops/CipherElite
-#
-#  License:        MIT
-#
-#  IMPORTANT:
-#    • If you copy, fork, or include this plugin in your own bot,
-#      you MUST keep this header intact.
-#    • You MUST give proper credit to the CipherElite Userbot author:
-#        – GitHub:    https://github.com/rishabhops/CipherElite
-#        – Telegram:  @thanosceo
-#
-#  Thank you for respecting open-source software!
-# =============================================================================
-
 from telethon import events, utils
 from utils.utils import CipherElite
 from utils.decorators import rishabh
